6e188918ba5eaa636e36635542ecaeefc1efec9e
BrannanC <brannanconrad@gmail.com>
2019-02-14 12:28:10 -0600

    Up and running

--- data/db.js
+++ data/db.js
@@ -1,5 +1,8 @@
 const knex = require('knex');
+require('dotenv').config();
 
-const config = require('../knexfile.js').development;
+const envType = process.env.DB_ENV || 'development';
+
+const config = require('../knexfile.js')[envType];
 
 module.exports = knex(config);
